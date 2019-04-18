330cd054417392dcb25c931d3d97bd483ef712ba
Logan S <logan.scott441@gmail.com>
2019-04-18 14:39:26 -0500

    Works pretty well now

--- facial_tracking/test_tracker.py
+++ facial_tracking/test_tracker.py
@@ -36,29 +36,58 @@ X_LOW_LIMIT = FRAME_CENTER_X - OFF_CENTER_LIMIT
 Y_HIGH_LIMIT = FRAME_CENTER_Y + OFF_CENTER_LIMIT
 Y_LOW_LIMIT = FRAME_CENTER_Y - OFF_CENTER_LIMIT
 
+#We want to track the position of this face every frame
+#to determine if we have to move the servos faster
+lastX = 0
+lastY = 0
+
+#Determine the limits for determining big/med/small move
+BIG_MOVE_LIMIT = 25
+MED_MOVE_LIMIT = 10
+
 def determineMoves(centerX, centerY):
     global panServo
     global tiltServo
+    global lastX
+    global lastY
     serialString = ""
+    print(centerX - lastX)
 
     if(centerY < Y_LOW_LIMIT):
         serialString += "down"
-        panServo = panServo - 1
     elif(centerY > Y_HIGH_LIMIT):
         serialString += "up"
-        panServo = panServo + 1
 
     if(centerX < X_LOW_LIMIT):
         serialString += "left"
-        tiltServo = tiltServo - 1
     elif(centerX > X_HIGH_LIMIT):
         serialString += "right"
-        tiltServo = tiltServo + 1
 
-    printString = "Pan servo: " + str(panServo) + ", " + "Tilt servo: " + str(tiltServo)
-    print(printString)
+    #Determine how far our face has moved
+    if(centerX - lastX > BIG_MOVE_LIMIT or -(centerX -lastX) > BIG_MOVE_LIMIT):
+        print("big x move")
+        serialString += "bigx"
+    elif(centerX - lastX > MED_MOVE_LIMIT or -(centerX - lastX) > MED_MOVE_LIMIT):
+        print("med x move")
+        serialString += "medx"
+    else:
+        print("small x move")
+        serialString += "smallx"
+
+    if(centerY - lastY > BIG_MOVE_LIMIT or -(centerY -lastY) > BIG_MOVE_LIMIT):
+        print("big y move")
+        serialString += "bigy"
+    elif(centerY - lastY > MED_MOVE_LIMIT or -(centerY - lastY) > MED_MOVE_LIMIT):
+        print("med y move")
+        serialString += "medy"
+    else:
+        print("small y move")
+        serialString += "smally"    
 
     sendToSerial(serialString) 
+
+    lastY = centerY
+    lastX = centerX
 
 def sendToSerial(data):
     duino.write(data.encode())
@@ -78,43 +107,47 @@ while (True):
     faces = face_cascade.detectMultiScale(gray, 1.125, 5)
     profiles = profile_cascade.detectMultiScale(gray, 1.1, 5)
 
-    for (x, y, w, h) in faces:		
-        cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        # I want to draw a line on the center of the face
-        pt1x = int(x + (w / 2) + 5)
-        pt1y = int(y + (h / 2) + 5)
-
-        pt2x = int(x + (w / 2) - 5)
-        pt2y = int(y + (h / 2) - 5)
-
-        # Point for center of image
-        centerX = int(x + (w / 2))
-        centerY = int(y + (h / 2))
-
-        cv2.line(gray,(pt1x,pt2y),(pt2x,pt1y),(255,0,0),3)
-
-        determineMoves(centerX,centerY)
-
-
-    if (len(faces) == 0): # only looks for profiles if a face hasn't been found
-        for (px, py, pw, ph) in profiles:
-            cv2.rectangle(gray,(px,py),(px+pw,py+ph),(0,255,0),2)
+    for i, (x, y, w, h) in enumerate(faces):	
+        
+        if(i == 0):	
+            cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
             # I want to draw a line on the center of the face
-            pt1x = int(px + (pw / 2) + 5)
-            pt1y = int(py + (ph / 2) + 5)
+            pt1x = int(x + (w / 2) + 5)
+            pt1y = int(y + (h / 2) + 5)
 
-            pt2x = int(px + (pw / 2) - 5)
-            pt2y = int(py + (ph / 2) - 5)
+            pt2x = int(x + (w / 2) - 5)
+            pt2y = int(y + (h / 2) - 5)
 
             # Point for center of image
-            centerX = int(px + (pw / 2))
-            centerY = int(py + (ph / 2))
+            centerX = int(x + (w / 2))
+            centerY = int(y + (h / 2))
 
             cv2.line(gray,(pt1x,pt2y),(pt2x,pt1y),(255,0,0),3)
 
             determineMoves(centerX,centerY)
+
+
+    if (len(faces) == 0): # only looks for profiles if a face hasn't been found
+        for i, (px, py, pw, ph) in enumerate(profiles):
+            #Only do it for the first face
+            if(i == 0):
+                cv2.rectangle(gray,(px,py),(px+pw,py+ph),(0,255,0),2)
+
+                # I want to draw a line on the center of the face
+                pt1x = int(px + (pw / 2) + 5)
+                pt1y = int(py + (ph / 2) + 5)
+
+                pt2x = int(px + (pw / 2) - 5)
+                pt2y = int(py + (ph / 2) - 5)
+
+                # Point for center of image
+                centerX = int(px + (pw / 2))
+                centerY = int(py + (ph / 2))
+
+                cv2.line(gray,(pt1x,pt2y),(pt2x,pt1y),(255,0,0),3)
+
+                determineMoves(centerX,centerY)
 
 	
     cv2.imshow('frame',gray)
