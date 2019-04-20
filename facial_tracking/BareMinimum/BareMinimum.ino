79681aa64749cf11e2504b847b452fb81be5c1a0
Jonathon Repta <31907193+JRapt0r@users.noreply.github.com>
2019-04-19 20:56:01 -0500

    Cleaned up code + minor adjustments

--- facial_tracking/BareMinimum/BareMinimum.ino
+++ facial_tracking/BareMinimum/BareMinimum.ino
@@ -1,78 +1,83 @@
 #include <Servo.h>
 
-//Up down servo
-Servo tiltServo;
+Servo tiltServo; // Up-down servo
+Servo panServo; // Left-right servo
 
-//Left right servo
-Servo panServo;
-
-int pan, tilt, multi;
-
+int pan, tilt, movementDegree;
 bool moveRight, moveLeft, moveUp, moveDown = false;
-void SetMovement(String data){
-    if(data.indexOf("left") > -1){
-      moveLeft = true;
-    }else if(data.indexOf("right") > -1){
-      moveRight = true;
+
+void setMovement(String data)
+{
+    if (data.indexOf("left") > -1) {
+        moveLeft = true;
+    }
+    else if (data.indexOf("right") > -1) {
+        moveRight = true;
     }
 
-    if(data.indexOf("up") > -1){
-      moveUp = true;
-    }else if(data.indexOf("down") > -1){
-      moveDown = true;
+    if (data.indexOf("up") > -1) {
+        moveUp = true;
+    }
+    else if (data.indexOf("down") > -1) {
+        moveDown = true;
     }
 
-    if(data.indexOf("high") > -1){
-      multi = 5;
-    }else if(data.indexOf("med") > -1){
-      multi = 2;
-    }else{
-      multi = 1;
+    if (data.indexOf("high") > -1) {
+        movementDegree = 7;
+    }
+    else if (data.indexOf("med") > -1) {
+        movementDegree = 4;
+    }
+    else {
+        movementDegree = 2;
     }
 }
 
-void ResetMove(){
-  moveLeft = false;
-  moveRight = false;
-  moveUp = false;
-  moveDown = false;
+void resetMove()
+{
+    moveLeft = false;
+    moveRight = false;
+    moveUp = false;
+    moveDown = false;
 }
 
-void MoveServos(){
-  if(moveRight){
-    panServo.write(panServo.read() + multi);
-  }else if(moveLeft){
-    panServo.write(panServo.read() - multi);
-  }
+void moveServos()
+{
+    if (moveRight) {
+        panServo.write(panServo.read() + movementDegree);
+    }
+    else if (moveLeft) {
+        panServo.write(panServo.read() - movementDegree);
+    }
 
-  if(moveUp){
-    tiltServo.write(tiltServo.read() + multi);
-  }else if(moveDown){
-    tiltServo.write(tiltServo.read() - multi);
-  }
+    if (moveUp) {
+        tiltServo.write(tiltServo.read() - movementDegree);
+    }
+    else if (moveDown) {
+        tiltServo.write(tiltServo.read() + movementDegree);
+    }
 }
 
-void setup() {
-  //For debugging
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-  Serial.setTimeout(50);
+void setup()
+{
+    // For debugging
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    Serial.setTimeout(50);
 
-  //Attach our tilt servo to 7
-  tiltServo.attach(7);
+    panServo.attach(6); // Attach pan servo to pin 6
+    tiltServo.attach(7); // Attach tilt servo to pin 7
 
-  //Attach our pan servo to 6
-  panServo.attach(6);
-
-  panServo.write(90);
-  tiltServo.write(90);
+    panServo.write(90);
+    tiltServo.write(90);
 }
 
-void loop() {
-  if(Serial.available() > 0){
-    String data = Serial.readString();
-    SetMovement(data);
-    MoveServos();
-    ResetMove();
-  }
+void loop()
+{
+    if (Serial.available() > 0) {
+        String data = Serial.readString();
+        setMovement(data);
+        moveServos();
+        resetMove();
+    }
 }
