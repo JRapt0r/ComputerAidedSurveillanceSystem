0592c56eef1eadbd08d0c6850a4a403dda4ab7bf
Jonathon Repta <31907193+JRapt0r@users.noreply.github.com>
2019-04-30 15:31:06 -0500

    Fixed typos + formatting

--- facial_tracking/arduino_code/io_duino.ino
+++ facial_tracking/arduino_code/io_duino.ino
@@ -7,15 +7,15 @@
 */
 
 //Include the liquid crystal library
-#include<LiquidCrystal.h>
+#include <LiquidCrystal.h>
 
 //Set up the LCD
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 //Pins for other IO
-buttonPin = 7;
-ledPin = 6;
+const int buttonPin = 7;
+const int ledPin = 6;
 
 //Stores the time of the last button press
 unsigned long lastButtonPress = 0;
@@ -23,9 +23,10 @@ unsigned long lastButtonPress = 0;
 //Half a second delay for button presses
 unsigned long debounceDelay = 500;
 
-void setup(){
+void setup()
+{
     //Start the lcd
-    lcd.begin(16,2);
+    lcd.begin(16, 2);
 
     //Print the default message of 0 faces
     lcd.print("0 faces detected");
@@ -38,30 +39,31 @@ void setup(){
     Serial.begin(115200);
 }
 
-void loop(){
+void loop()
+{
     //Stores the reading from the button
     int buttonReading = digitalRead(buttonPin);
 
     //If the button is pressed
     //And we have waited long enough since the last press
-    if(reading == HIGH && (millis() - lastButtonPress) > debounceDelay){
+    if (buttonReading == HIGH && (millis() - lastButtonPress) > debounceDelay) {
         //Send serial information to our python program
-        Serial.write("toggleTracking")
+        Serial.write("toggleTracking");
 
         //Set the last buttonPress time
         lastButtonPress = millis();
     }
 
     //If we recieved serial data
-        if (Serial.available() > 0) {
+    if (Serial.available() > 0) {
         String data = Serial.readString();
         //Determine if our data is related to detected faces or LED
-        if(data.indexOf("toggleLED") > -1){
+        if (data.indexOf("toggleLED") > -1) {
             digitalWrite(ledPin, HIGH);
-        }else if(data.indexOf("faceData") > -1){
-            data = data.subString(16);
+        }
+        else if (data.indexOf("faceData") > -1) {
+            data = data.substring(16);
             lcd.print(data);
         }
     }
-
 }
